4700bb5269cb31b422f1e580c43e1a2f0fd1e0f6
dougc89 <dougc89@vt.edu>
2022-12-11 00:18:02 -0500

    template looks fine

---
+++ wwwroot/meta/css/mdb_css-package.php
@@ -0,0 +1,13 @@
+<?php //CSS INCLUDES: ?>
+<!--GIJGO Datepicker-->
+<link rel="stylesheet" href="/meta/plugins/gijgo_datepicker/gijgo.min.css" type="text/css" />
+<!-- Font Awesome -->
+<link rel="stylesheet" href="/meta/plugins/fontawesome/css/all.css" type="text/css">
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" href='/meta/plugins/mdbootstrap/css/bootstrap.min.css' type="text/css" />
+<!-- Material Design Bootstrap Styles-->
+<link rel="stylesheet" href='/meta/plugins/mdbootstrap/css/mdb.min.css' type="text/css" />
+<!--Material Design Bootstrap Datatables Addon-->
+<link rel="stylesheet" href="/meta/plugins/mdbootstrap/css/addons/datatables.min.css" type="text/css">
+<!--Quill.js Rich text editor-->
+<link rel="stylesheet" href="/meta/plugins/quill/quill.snow.css" >
